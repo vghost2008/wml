15385e17d1b9f706d7ccd925914ad2c0b5cad074
vghost2008 <bluetornado@zju.edu.cn>
2023-01-19 16:42:40 +0800

    Update multi data loader log info.

--- wtorch/data/multi_processing_data_loader_iter.py
+++ wtorch/data/multi_processing_data_loader_iter.py
@@ -330,8 +330,8 @@ class _MultiProcessingDataLoaderIter(_BaseDataLoaderIter):
     #     down.
 
     def __init__(self, loader,batch_split_nr=1):
-        print(f"Use _WMultiProcessingDataLoaderIter, batch split nr = {batch_split_nr}, pin memory= {loader.pin_memory}")
         super(_MultiProcessingDataLoaderIter, self).__init__(loader)
+        print(f"Use _WMultiProcessingDataLoaderIter, batch split nr = {batch_split_nr}, pin memory= {loader.pin_memory}, num_works= {self._num_workers}")
 
         assert self._num_workers > 0
         assert self._prefetch_factor > 0
