673177ecfcd5a36e9c64fa14e06cadb27239c618
vghost2008 <bluetornado@zju.edu.cn>
2023-11-17 21:22:37 +0800

    Update metrics toolkit.

--- object_detection2/metrics/toolkit.py
+++ object_detection2/metrics/toolkit.py
@@ -14,6 +14,14 @@ from pycocotools.cocoeval import COCOeval
 import wml_utils as wmlu
 from .build import METRICS_REGISTRY
 
+class BaseMetrics:
+    def __init__(self) -> None:
+        self._current_info = ""
+        pass
+
+    def current_info(self):
+        return self._current_info
+
 def __safe_persent(v0,v1):
     if v1==0:
         return 100.
@@ -61,7 +69,7 @@ probability:[Y], the probability for boxes, if probability is none, assum the bo
 return:
 mAP:[0,100]
 '''
-def getmAP(gtboxes,gtlabels,boxes,labels,probability=None,threshold=0.5):
+def getmAP(gtboxes,gtlabels,boxes,labels,probability=None,threshold=0.5,is_crowd=None):
 
     if not isinstance(gtboxes,np.ndarray):
         gtboxes = np.array(gtboxes)
@@ -71,6 +79,10 @@ def getmAP(gtboxes,gtlabels,boxes,labels,probability=None,threshold=0.5):
         boxes = np.array(boxes)
     if not isinstance(labels,np.ndarray):
         labels = np.array(labels)
+    if is_crowd is None:
+        is_crowd = np.zeros([gtlabels.shape[0]],dtype=np.bool)
+    if not isinstance(is_crowd,np.ndarray):
+        is_crowd = np.array(is_crowd)
     gtboxes = copy.deepcopy(np.array(gtboxes))
     gtlabels = copy.deepcopy(np.array(gtlabels))
     boxes = copy.deepcopy(boxes)
@@ -92,29 +104,42 @@ def getmAP(gtboxes,gtlabels,boxes,labels,probability=None,threshold=0.5):
     else:
         beg_index = range(0,data_nr)
 
-    res = []
+    t_res = []
 
     for v in beg_index:
-        p,r = getPrecision(gtboxes,gtlabels,boxes[v:],labels[v:],threshold)
-        res.append([p,r])
+        p,r = getPrecision(gtboxes,gtlabels,boxes[v:],labels[v:],threshold,is_crowd=is_crowd)
+        t_res.append([p,r])
 
-    res.sort(key=lambda x:x[1])
-    #print("mAP:res: {}".format(res))
+
+    t_res1 = []
+    old_v = None
+    for v in t_res:
+        if old_v is not None and v[0]<old_v[0]:
+            v[0] = old_v[0]
+        t_res1.append(v)
+        old_v = v
+
+    res = []
+    old_v = None
+    for v in reversed(t_res1):
+        if old_v is not None and v[1]<old_v[1]:
+            v[1] = old_v[1]
+        res.append(v)
 
     min_r = res[0][1]
     max_r = res[-1][1]
     logging.debug("mAP: max r {}, min r {}".format(max_r,min_r))
     if max_r-min_r<1.0:
-        p,r = getPrecision(gtboxes,gtlabels,boxes,labels,threshold)
+        p,r = getPrecision(gtboxes,gtlabels,boxes,labels,threshold,is_crowd=is_crowd)
         res = [[p,r],[p,r]]
 
     if min_r > 1e-2:
         res = np.concatenate([np.array([[res[0][0],0.]]),res],axis=0)
     if max_r <100.0-1e-2:
-        if max_r+10.<100.0:
-            res = np.concatenate([res,np.array([[0.,max_r+10.],[0.,100.]])])
-        else:
-            res = np.concatenate([res,np.array([[0.,max_r+10.]])])
+        l_precisions = res[-1][0]
+        l_recall = res[-1][1]
+        t_precision = min(l_precisions*l_recall/100.0,l_precisions)
+        res = np.concatenate([res,np.array([[t_precision,100.0]])])
 
     res = np.array(res)
     res = res.transpose()
@@ -125,9 +150,6 @@ def getmAP(gtboxes,gtlabels,boxes,labels,probability=None,threshold=0.5):
     for r in new_r:
         new_p.append(np.interp(r,recall,precisions))
     precisions = np.array(new_p)
-    if precisions.shape[0]>1:
-        for i in range(precisions.shape[0]-1):
-            precisions[i] = np.max(precisions[i+1:])
     return np.mean(precisions)
 
 
@@ -166,7 +188,7 @@ def getRecall(gtboxes,gtlabels,boxes,labels,threshold=0.5):
 
     return 100.*correct_num/total_num
 
-def getAccuracy(gtboxes,gtlabels,boxes,labels,threshold=0.5,auto_scale_threshold=True,ext_info=False):
+def getAccuracy(gtboxes,gtlabels,boxes,labels,threshold=0.5,auto_scale_threshold=True,ext_info=False,is_crowd=None):
     '''
     :param gtboxes: [N,4]
     :param gtlabels: [N]
@@ -179,6 +201,10 @@ def getAccuracy(gtboxes,gtlabels,boxes,labels,threshold=0.5,auto_scale_threshold
         gtboxes = np.array(gtboxes)
     if not isinstance(gtlabels,np.ndarray):
         gtlabels = np.array(gtlabels)
+    if is_crowd is None:
+        is_crowd = np.zeros([gtlabels.shape[0]],dtype=np.bool)
+    if not isinstance(is_crowd,np.ndarray):
+        is_crowd = np.array(is_crowd)
     gt_shape = gtboxes.shape
     #indict if there have some box match with this ground-truth box
     gt_mask = np.zeros([gt_shape[0]],dtype=np.int32)
@@ -214,17 +240,19 @@ def getAccuracy(gtboxes,gtlabels,boxes,labels,threshold=0.5,auto_scale_threshold
         gt_mask[i] = 1
         boxes_mask[max_index] = 1
 
+    r_gt_mask = np.logical_or(gt_mask,is_crowd)
+    correct_gt_num = np.sum(r_gt_mask)
+    correct_bbox_num = np.sum(boxes_mask)
     correct_num = np.sum(gt_mask)
+    r_gt_size = gt_size-correct_gt_num+correct_bbox_num
 
-    recall = __safe_persent(correct_num,gt_size)
-    precision = __safe_persent(correct_num,boxes_size)
     P_v = gt_size
-    TP_v = correct_num
+    TP_v = correct_bbox_num
     FP_v = boxes_size-correct_num
 
-    return __safe_persent(TP_v,gt_size+boxes_size-TP_v)
+    return __safe_persent(TP_v,r_gt_size+boxes_size-correct_bbox_num)
 
-def getPrecision(gtboxes,gtlabels,boxes,labels,threshold=0.5,auto_scale_threshold=True,ext_info=False):
+def getPrecision(gtboxes,gtlabels,boxes,labels,threshold=0.5,auto_scale_threshold=True,ext_info=False,is_crowd=None):
     '''
     :param gtboxes: [N,4]
     :param gtlabels: [N]
@@ -237,6 +265,10 @@ def getPrecision(gtboxes,gtlabels,boxes,labels,threshold=0.5,auto_scale_threshol
         gtboxes = np.array(gtboxes)
     if not isinstance(gtlabels,np.ndarray):
         gtlabels = np.array(gtlabels)
+    if is_crowd is None:
+        is_crowd = np.zeros([gtlabels.shape[0]],dtype=np.bool)
+    if not isinstance(is_crowd,np.ndarray):
+        is_crowd = np.array(is_crowd)
     gt_shape = gtboxes.shape
     #indict if there have some box match with this ground-truth box
     gt_mask = np.zeros([gt_shape[0]],dtype=np.int32)
@@ -273,13 +305,15 @@ def getPrecision(gtboxes,gtlabels,boxes,labels,threshold=0.5,auto_scale_threshol
         gt_mask[i] = 1
         boxes_mask[max_index] = 1
 
-    correct_num = np.sum(gt_mask)
+    r_gt_mask = np.logical_or(gt_mask,is_crowd)
+    correct_gt_num = np.sum(r_gt_mask)
+    correct_bbox_num = np.sum(boxes_mask)
 
-    recall = __safe_persent(correct_num,gt_size)
-    precision = __safe_persent(correct_num,boxes_size)
+    recall = __safe_persent(correct_gt_num,gt_size)
+    precision = __safe_persent(correct_bbox_num,boxes_size)
     P_v = gt_size
-    TP_v = correct_num
-    FP_v = boxes_size-correct_num
+    TP_v = correct_bbox_num
+    FP_v = boxes_size-correct_bbox_num
 
 
     if ext_info:
@@ -419,11 +453,12 @@ def getPrecisionV2(gt_data,pred_data,pred_func,threshold,return_f1=False):
     return precision,recall
 
 @METRICS_REGISTRY.register()
-class Accuracy:
+class Accuracy(BaseMetrics):
     def __init__(self,threshold=0.1,num_classes=90,label_trans=None,classes_begin_value=1,*args,**kwargs):
         self.threshold = threshold
         self.gtboxes = []
         self.gtlabels = []
+        self.is_crowd = []
         self.boxes = []
         self.labels = []
         self.precision = None
@@ -431,6 +466,7 @@ class Accuracy:
         self.total_test_nr = 0
         self.num_classes = num_classes
         self.label_trans = label_trans
+        self.bboxes_offset = np.zeros([1,4],dtype=np.float32)
         del classes_begin_value
 
     def __call__(self, gtboxes,gtlabels,boxes,labels,probability=None,img_size=[512,512],
@@ -440,12 +476,23 @@ class Accuracy:
             gtlabels = self.label_trans(gtlabels)
             labels = self.label_trans(labels)
         if gtboxes.shape[0]>0:
-            self.gtboxes.append(gtboxes)
-            self.gtlabels.append(np.array(gtlabels)+self.total_test_nr*self.num_classes)
+            self.gtboxes.append(np.array(gtboxes)+self.bboxes_offset)
+            self.gtlabels.append(np.array(gtlabels))
+            if is_crowd is None:
+                is_crowd = np.zeros([gtlabels.shape[0]],dtype=np.bool)
+            self.is_crowd.append(np.array(is_crowd))
         if boxes.shape[0]>0:
-            self.boxes.append(boxes)
-            self.labels.append(np.array(labels)+self.total_test_nr*self.num_classes)
+            self.boxes.append(np.array(boxes)+self.bboxes_offset)
+            self.labels.append(np.array(labels))
+
         self.total_test_nr += 1
+
+        t_bboxes = np.concatenate([gtboxes,boxes],axis=0)
+        t_max = np.max(t_bboxes,axis=0)
+        max_0 = t_max[2]
+        max_1 = t_max[3]
+        offset = np.array([[max_0,max_1,max_0,max_1]],dtype=np.float32)
+        self.bboxes_offset = self.bboxes_offset+offset
 
     def evaluate(self):
         if self.total_test_nr==0 or len(self.boxes)==0 or len(self.labels)==0:
@@ -456,7 +503,9 @@ class Accuracy:
         boxes = np.concatenate(self.boxes,axis=0)
         labels = np.concatenate(self.labels,axis=0)
         self.acc = getAccuracy(gtboxes, gtlabels, boxes, labels, threshold=self.threshold,
-                                                  auto_scale_threshold=False, ext_info=False)
+                                                  auto_scale_threshold=False,
+                                                  ext_info=False,
+                                                  is_crowd=self.is_crowd)
     def show(self,name=""):
         self.evaluate()
         res = f"{name}: total test nr {self.total_test_nr}, acc {self.acc:.3f}"
@@ -469,32 +518,46 @@ class Accuracy:
             return "N.A."
 
 @METRICS_REGISTRY.register()
-class PrecisionAndRecall:
+class PrecisionAndRecall(BaseMetrics):
     def __init__(self,threshold=0.5,num_classes=90,label_trans=None,classes_begin_value=1,*args,**kwargs):
         self.threshold = threshold
         self.gtboxes = []
         self.gtlabels = []
         self.boxes = []
         self.labels = []
+        self.is_crowd = []
         self.precision = None
         self.recall = None
         self.total_test_nr = 0
         self.num_classes = num_classes
         self.label_trans = label_trans
+        self.bboxes_offset = np.zeros([1,4],dtype=np.float32)
         del classes_begin_value
 
     def __call__(self, gtboxes,gtlabels,boxes,labels,probability=None,img_size=[512,512],
                  gtmasks=None,
                  masks=None,is_crowd=None,use_relative_coord=True):
+
         if self.label_trans is not None:
             gtlabels = self.label_trans(gtlabels)
             labels = self.label_trans(labels)
+
         if gtboxes.shape[0]>0:
-            self.gtboxes.append(gtboxes)
-            self.gtlabels.append(np.array(gtlabels)+self.total_test_nr*self.num_classes)
+            self.gtboxes.append(gtboxes+self.bboxes_offset)
+            self.gtlabels.append(np.array(gtlabels))
+            self.is_crowd.append(np.array(is_crowd))
+
         if boxes.shape[0]>0:
-            self.boxes.append(boxes)
-            self.labels.append(np.array(labels)+self.total_test_nr*self.num_classes)
+            self.boxes.append(boxes+self.bboxes_offset)
+            self.labels.append(np.array(labels))
+        
+        t_bboxes = np.concatenate([gtboxes,boxes],axis=0)
+        t_max = np.max(t_bboxes,axis=0)
+        max_0 = t_max[2]
+        max_1 = t_max[3]
+        offset = np.array([[max_0,max_1,max_0,max_1]],dtype=np.float32)
+        self.bboxes_offset = self.bboxes_offset+offset
+
         self.total_test_nr += 1
 
     def evaluate(self):
@@ -504,13 +567,17 @@ class PrecisionAndRecall:
         if len(self.gtboxes) == 0:
             gtboxes = np.zeros([0,4],dtype=np.float32)
             gtlabels = np.zeros([0],dtype=np.int32)
+            is_crowd = np.zeros([0],dtype=np.bool)
         else:
             gtboxes = np.concatenate(self.gtboxes,axis=0)
             gtlabels = np.concatenate(self.gtlabels,axis=0)
+            is_crowd = np.concatenate(self.is_crowd,axis=0).astype(np.bool)
         boxes = np.concatenate(self.boxes,axis=0)
         labels = np.concatenate(self.labels,axis=0)
         self.precision,self.recall = getPrecision(gtboxes, gtlabels, boxes, labels, threshold=self.threshold,
-                                                  auto_scale_threshold=False, ext_info=False)
+                                                  auto_scale_threshold=False,
+                                                  ext_info=False,
+                                                  is_crowd=is_crowd)
     @property
     def f1(self):
         return 2*self.precision*self.recall/max(self.precision+self.recall,1e-8)
@@ -534,7 +601,7 @@ class PrecisionAndRecall:
         return res
 
 @METRICS_REGISTRY.register()
-class EasyPrecisionAndRecall:
+class EasyPrecisionAndRecall(BaseMetrics):
     def __init__(self,threshold=0.05,num_classes=90,label_trans=None,classes_begin_value=1,*args,**kwargs):
         self.threshold = threshold
         self.gtboxes = []
@@ -595,13 +662,9 @@ class EasyPrecisionAndRecall:
         return res
 
 @METRICS_REGISTRY.register()
-class ImgLevelPrecisionAndRecall:
+class ImgLevelPrecisionAndRecall(BaseMetrics):
     def __init__(self,threshold=0.5,num_classes=90,label_trans=None,classes_begin_value=1,*args,**kwargs):
         self.threshold = threshold
-        self.gtboxes = []
-        self.gtlabels = []
-        self.boxes = []
-        self.labels = []
         self.precision = None
         self.recall = None
         self.total_test_nr = 0
@@ -618,15 +681,37 @@ class ImgLevelPrecisionAndRecall:
         if self.label_trans is not None:
             gtlabels = self.label_trans(gtlabels)
             labels = self.label_trans(labels)
+        if not isinstance(gtlabels,np.ndarray):
+            gtlabels = np.array(gtlabels)
+        ori_gtlabels = gtlabels.copy()
         gtlabels = set(gtlabels)
         labels = set(labels)
-        tp = len(gtlabels&labels)
-        fn = len(gtlabels)-tp
+        union_labels = gtlabels&labels
+        tp = len(union_labels)
+        false_negative = gtlabels-union_labels
+        fn = len(false_negative)
+
+        if is_crowd is not None:
+            for l in false_negative:
+                mask = ori_gtlabels==l
+                t_is_crowd = is_crowd[mask]
+                if np.all(t_is_crowd):
+                    fn = fn-1
+
         fp = len(labels)-tp
         self.tp += tp
         self.fn += fn
         self.fp += fp
         self.total_test_nr += 1
+        if fp+tp>0:
+            precision = tp/(fp+tp)
+        else:
+            precision = 1.0
+        if fn+tp>0:
+            recall = tp/(fn+tp)
+        else:
+            recall = 1.0
+        self._current_info = f"precision={precision:.3f}, recall={recall:.3f}"
 
         
 
@@ -778,7 +863,7 @@ class ModelPerformance:
         elif item=="precision":
             return self.safe_div(self.total_precision,self.test_nr)
 
-class GeneralCOCOEvaluation(object):
+class GeneralCOCOEvaluation(BaseMetrics):
     def __init__(self,categories_list=None,
                  num_classes=None,mask_on=False,label_trans=None,
                  classes_begin_value=1,
@@ -805,6 +890,7 @@ class GeneralCOCOEvaluation(object):
     gtlabels:[N]
     img_size:[H,W]
     gtmasks:[N,H,W]
+    is_crowd: [N]
     '''
     def __call__(self, gtboxes,gtlabels,boxes,labels,probability=None,img_size=[512,512],
                  gtmasks=None,
@@ -812,6 +898,14 @@ class GeneralCOCOEvaluation(object):
         if self.min_bbox_size > 0:
             gtboxes = odb.clamp_bboxes(gtboxes,self.min_bbox_size)
             boxes = odb.clamp_bboxes(boxes,self.min_bbox_size)
+        
+        cur_ap = getmAP(gtboxes=gtboxes,
+                        gtlabels=gtlabels,
+                        boxes=boxes,
+                        labels=labels,
+                        probability=probability,
+                        is_crowd=is_crowd)
+        self._current_info = f"ap={cur_ap}"
 
         if probability is None:
             probability = np.ones_like(labels,dtype=np.float32)
@@ -933,7 +1027,7 @@ class COCOMaskEvaluation(GeneralCOCOEvaluation):
                          **kwargs)
 
 @METRICS_REGISTRY.register()
-class COCOEvaluation(object):
+class COCOEvaluation(BaseMetrics):
     '''
     num_classes: 不包含背景 
     '''
@@ -952,8 +1046,10 @@ class COCOEvaluation(object):
                                                      **kwargs)
     def __call__(self, *args, **kwargs):
         self.box_evaluator(*args,**kwargs)
+        self._current_info = self.box_evaluator.current_info()
         if self.mask_evaluator is not None:
             self.mask_evaluator(*args,**kwargs)
+            self._current_info += ", mask" + self.mask_evaluator.current_info()
 
     def num_examples(self):
         return self.box_evaluator.num_examples()
@@ -977,7 +1073,7 @@ class COCOEvaluation(object):
             return self.box_evaluator.to_string()
 
 @METRICS_REGISTRY.register()
-class COCOKeypointsEvaluation(object):
+class COCOKeypointsEvaluation(BaseMetrics):
     def __init__(self,num_joints,categories="person",oks_sigmas=None):
         categories_keypoints = []
         for i in range(num_joints):
@@ -1089,7 +1185,7 @@ class COCOKeypointsEvaluation(object):
         else:
             return f"N.A."
 
-class ClassesWiseModelPerformace(object):
+class ClassesWiseModelPerformace(BaseMetrics):
     def __init__(self,num_classes,threshold=0.5,classes_begin_value=1,model_type=COCOEvaluation,model_args={},label_trans=None,
                  **kwargs):
         self.num_classes = num_classes
@@ -1201,7 +1297,7 @@ class ClassesWiseModelPerformace(object):
         elif item=="precision":
             return self.mp.precision
 
-class SubsetsModelPerformace(object):
+class SubsetsModelPerformace(BaseMetrics):
     def __init__(self, num_classes, sub_sets,threshold=0.5, model_type=COCOEvaluation, classes_begin_value=1,model_args={},
                  label_trans=None,
                  **kwargs):
@@ -1262,6 +1358,9 @@ class SubsetsModelPerformace(object):
             self.data[i](lgtboxes, lgtlabels, lboxes, llabels, lprobs,img_size=img_size)
         return self.mp(gtboxes, gtlabels, boxes, labels)
 
+    def current_info(self):
+        return self.mp.current_info()
+
     def show(self):
         for i,sub_set_labels in enumerate(self.sub_sets):
             print(f"Classes:{sub_set_labels}")
@@ -1286,7 +1385,7 @@ class SubsetsModelPerformace(object):
             return self.mp.precision
 
 @METRICS_REGISTRY.register()
-class  MeanIOU(object):
+class  MeanIOU(BaseMetrics):
     def __init__(self,num_classes,*args,**kwargs):
         self.intersection = np.zeros(shape=[num_classes],dtype=np.int64)
         self.union = np.zeros(shape=[num_classes],dtype=np.int64)
@@ -1358,7 +1457,7 @@ def coco_bbox_eval_file(gt_file, res_file):
 
     return info_str
 
-class WMAP(object):
+class WMAP(BaseMetrics):
     def __init__(self,categories_list=None,num_classes=None,mask_on=False,label_trans=None,classes_begin_value=1,threshold=0.5):
         if categories_list is None:
             print(f"WARNING: Use default categories list, start classes is {classes_begin_value}")
@@ -1400,6 +1499,15 @@ class WMAP(object):
             labels = self.label_trans(labels)
         if probability is not None and not isinstance(probability,np.ndarray):
             probability = np.array(probability)
+
+        cur_ap = getmAP(gtboxes=gtboxes,
+                        gtlabels=gtlabels,
+                        boxes=boxes,
+                        labels=labels,
+                        probability=probability,
+                        is_crowd=is_crowd)
+        self._current_info = f"ap={cur_ap}"
+
         if gtlabels.shape[0]>0:
             if use_relative_coord:
                 gtboxes = gtboxes*[[img_size[0],img_size[1],img_size[0],img_size[1]]]
