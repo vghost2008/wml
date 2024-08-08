affec7ab0cf6721af4b3accc9b6bbf1846ea0322
vghost2008 <bluetornado@zju.edu.cn>
2024-08-08 10:02:58 +0800

    Update dropblock.

--- wtorch/dropblock/__init__.py
+++ wtorch/dropblock/__init__.py
@@ -1,5 +1,6 @@
-from .dropblock import DropBlock2D, DropBlock3D
+from .dropblock import DropBlock2D, WDropBlock2D, DropBlock3D
 from .scheduler import LinearScheduler
 from .dropout import WDropout
 
-__all__ = ['DropBlock2D', 'DropBlock3D', 'LinearScheduler','WDropout']
+
+__all__ = ['DropBlock2D', 'DropBlock3D', 'LinearScheduler','WDropout', 'WDropBlock2D']
--- wtorch/dropblock/dropblock.py
+++ wtorch/dropblock/dropblock.py
@@ -86,6 +86,74 @@ class DropBlock2D(nn.Module):
         return self.drop_prob / (self.block_size ** 2)
 
 
+class WDropBlock2D(nn.Module):
+    r"""
+    与DropBlock2D不同的在于：DropBlock2d会将一个空间位置整个通道都设置为0，或者都设置为1,
+    WDropBlock2D会将channel中的各元素单独drop
+    """
+
+    def __init__(self, drop_prob, block_size):
+        super().__init__()
+
+        self.drop_prob = drop_prob
+        self.block_size = block_size
+        self.cur_drop_prob = 0.0
+
+    def forward(self, x):
+        # shape: (bsize, channels, height, width)
+
+        assert x.dim() == 4, \
+            "Expected input with 4 dimensions (bsize, channels, height, width)"
+
+        if not self.training or self.drop_prob <= 0.:
+            self.cur_drop_prob = 0.0
+            return x
+        else:
+            # get gamma value
+            gamma = self._compute_gamma(x)
+
+            # sample mask
+            mask = (torch.rand(*x.shape) < gamma).to(x.dtype)
+
+            # place mask on input device
+            mask = mask.to(x.device)
+
+            # compute block mask
+            block_mask = self._compute_block_mask(mask)
+
+            cur_nr = torch.sum(block_mask,dtype=torch.float32)
+            if cur_nr < 1:
+                print(f"ERROR: DropBlock drop_prob={self.drop_prob}, gamma={gamma}, cur_nr={cur_nr}")
+                return x
+
+            scale = (block_mask.numel()/cur_nr).to(x.dtype)
+
+            # apply block mask
+            out = x * block_mask
+
+            # scale output
+            out = out * scale
+
+            self.cur_drop_prob = 1.0-cur_nr/block_mask.numel()
+            return out
+
+    def _compute_block_mask(self, mask):
+        block_mask = F.max_pool2d(input=mask,
+                                  kernel_size=(self.block_size, self.block_size),
+                                  stride=(1, 1),
+                                  padding=self.block_size // 2)
+
+        if self.block_size % 2 == 0:
+            block_mask = block_mask[:, :, :-1, :-1]
+
+        block_mask = 1 - block_mask
+
+        return block_mask
+
+    def _compute_gamma(self, x):
+        return self.drop_prob / (self.block_size ** 2)
+
+
 class DropBlock3D(DropBlock2D):
     r"""Randomly zeroes 3D spatial blocks of the input tensor.
 
